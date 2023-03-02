846c70a27380f73bdb33a7cc43d730b1fc8796dd
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-03-02 16:42:17 +0530

    Added: BinaryNodeTree.java file

---
+++ BinaryNodeTree.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.binarysearchtree;
+
+/**
+ * @author Dell
+ *
+ */
+public class BinaryNodeTree<T> {
+	T data;
+	BinaryNodeTree<T> left;
+	BinaryNodeTree<T> right;
+
+}
+
+
